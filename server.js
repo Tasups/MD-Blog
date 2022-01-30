26f78ae7ec3319cdd97ead4a344cf1b3b3cccfc7
Jason Whisnant <tasups@gmail.com>
2022-01-30 14:47:59 -0500

    create views/articles move index.ejs into it

--- server.js
+++ server.js
@@ -7,12 +7,21 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-  const articles = [{
-    title: 'Test Article',
-    createdAt: Date.now(),
-    description: 'Test description'
-  }]
-  res.render("index", { articles: articles });
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description:
+        "This is a description to test out the forEach method in regards to displaying information dynamically",
+    },
+    {
+      title: "Test Article TWO",
+      createdAt: new Date(),
+      description:
+        "This is a description to test out the forEach method in regards to displaying information dynamically. But this time it's for another object, so, there's that.",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
 })
 
 app.listen(5000)
